43d71f8432afe487dfd970c31be51262d5219bd8
Owner <owner@MacBook-Pro.local>
2017-11-06 16:50:38 -0500

    updated component

--- client/main.js
+++ client/main.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import Lander from '../components/landing/landing.js';
 
 
+//all components will be attached to this Page component. 
+//Page component will be rendered to the html file
+class Page extends Component {
+    render(){
+        return(
+            <h2>
+            <Lander />
+            </h2>
+        )
+    }
 
+}
 
-RenderDOM.render(<h1>Main.js</h1>, document.getElementById('mount'))
+RenderDOM.render(<h1>Main.js</h1>, document.getElementById('mount'))    
