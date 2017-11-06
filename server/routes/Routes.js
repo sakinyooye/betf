64dcc48315274b410f11be1cee93104e2235efc4
Seamus Martin <spmartin823@gmail.com>
2017-11-06 15:42:07 -0500

    small changes

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -7,12 +7,14 @@ module.exports = function(app) {
   app.get('/users', user.getAllUsers)
   app.post('/users', user.addAUser);
 
+  // routes to handle getting and updating specific user. 
+  app.get('/users/:username', user.getSpecificUserData)
   app.put('/users/:username', user.updateUser)
 
   // routes for games:
   app.get('/games', game.getAllGames)
   app.post('/games', game.addAGame);
-
+  
   app.put('/games/:id', game.updateGame)
 
   // routes algorithms: 
