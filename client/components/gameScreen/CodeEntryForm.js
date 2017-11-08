e8c8ecde27b9ca144b6c9059654c628d87653ce2
Seamus Martin <spmartin823@gmail.com>
2017-11-08 17:28:07 -0500

    stephen

--- client/components/gameScreen/CodeEntryForm.js
+++ client/components/gameScreen/CodeEntryForm.js
@@ -2,21 +2,35 @@
 // this file will just house the code entry. It takes the starterCode from app.js
 import React from 'react';
 import axios from 'axios'
+import brace from 'brace';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/javascript';
+import 'brace/theme/github';
 
 export class CodeEntryForm extends React.Component {
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props)
 
 
-	}
+  }
+  onChange(newValue) {
+    console.log('change',newValue);
+  }
 
-	render() {
+  render() {
     return (
       <div>
-
+        <AceEditor
+          mode="javascript"
+          theme="github"
+          onChange={this.onChange.bind(this)}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{$blockScrolling: true}}
+        />
       </div>
     )
-	}
+  }
 }
 
-export default CodeEntryForm; 
+export default CodeEntryForm;
